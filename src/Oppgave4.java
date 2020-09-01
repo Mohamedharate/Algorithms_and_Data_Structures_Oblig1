d9459013c12062d9a7b1d82bc94b270ac59a684f
AndreasTHansen <andreas-torres@hotmail.com>
2020-09-01 14:30:11 +0200

    Initial commit

---
+++ src/Oppgave4.java
@@ -0,0 +1,11 @@
+public class Oppgave4 {
+
+    public static void delsortering(int[] a){
+
+
+        for (int i= 0; i<a.length; i++){
+
+        }
+    }
+
+}
---
+++ src/Oppgave7b.java
@@ -0,0 +1,32 @@
+public class Oppgave7b {
+
+    public static String flett(String... s){
+
+
+        String resultat="";
+        int makslengde = s[0].length();
+
+        for (int k = 1; k<makslengde ; k++) { //for å finne den ytterste grensen til sjekken
+            if (s[k].length() > makslengde) {
+                makslengde = s[k].length();
+            }
+        }
+
+        for (int j=0; j<makslengde; j++){
+            for (int i = 0; i<s.length; i++){
+                if (j<s[i].toCharArray().length){
+                    resultat += "" + s[i].toCharArray()[j];
+                }
+            }
+        }
+
+        return resultat;
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(flett("abc","123","dfe"));
+
+    }
+
+}
